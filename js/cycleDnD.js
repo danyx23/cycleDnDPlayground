8684d401cdc40953cf438c0de9844c0c76ee42cc
Daniel Bachler <daniel@danielbachler.de>
2015-05-10 14:02:55 +0200

    Added a bunch of logging, fixed remaining bugs, basic dropping of files works now.

--- js/cycleDnD.js
+++ js/cycleDnD.js
@@ -1,30 +1,50 @@
 import Cycle from 'cyclejs';
 import cuid from 'cuid';
 const {h, Rx} = Cycle;
+import R from 'ramda';
 
 function intent(interactions) {
     return {
+        dragEnter$: interactions.get('.project-bin', 'dragenter')
+                            .map(eventData => {
+                                eventData.stopPropagation();
+                                eventData.preventDefault();
+                                eventData.dataTransfer.dropEffect = 'copy';
+                                return eventData.dataTransfer.files;
+                            }),
         dragOver$: interactions.get('.project-bin', 'dragover')
                            .map(eventData => {
+                                eventData.stopPropagation();
                                 eventData.preventDefault();
+                                eventData.dataTransfer.dropEffect = 'copy';
                                 return eventData.dataTransfer.files;
                             }),
         droppedFiles$: interactions.get('.project-bin', 'drop')
                           .map(eventData => {
+                                eventData.stopPropagation();
                                 eventData.preventDefault();
                                 return eventData.dataTransfer.files;
                             })
+                           .share()
     };
 };
 
+function arrayUpdateHelper(itemArray, item) {
+    const arrayCopy = itemArray.slice();
+    arrayCopy.push(item);
+    return arrayCopy;
+}
+
 function model(intent) {
     return {
-        isDragHovering$: intent.dragOver$.map(_ => true)
-                             .takeUntil(intent.droppedFiles$)
-                             .concat(Rx.Observable.just(false))
+        isDragHovering$: intent.dragOver$
+                             .merge(intent.dragEnter$)
+                             .map(_ => true)
+                             .merge(intent.droppedFiles$.map(_ => false))
                              .startWith(false),
         items$: intent.droppedFiles$.flatMap(files => {
                         const filesArray = [];
+                        console.log(`Items were dropped: ${files}`)
                         for (var i = 0; i < files.length; i++) {
                             filesArray.push(files[i]);
                         }
@@ -35,10 +55,12 @@ function model(intent) {
                             id: cuid(),
                             name: file.name
                         };
+                        console.log(`id: ${asset.id}, name: ${asset.name}`);
                         return asset;
                     })
-                    .scan([], (itemArray, item) => itemArray.push(item))
+                    .scan([], arrayUpdateHelper)
                     .startWith([])
+                    .tap(items => console.log(`items length is ${items.length}, the last element is ${R.last(items)}`))
     }
 }
 
@@ -46,14 +68,18 @@ function view(model) {
     return Rx.Observable.combineLatest(model.isDragHovering$,
                                        model.items$,
         (isDraghovering, items) => {
-            const vItems = items.map(item => {
+            console.log(items);
+            let vItems = items.map(item => {
                 return h('div.project-item', {key: item.id}, [
                     h('img.project-item__image'),
                     h('div.project-item__name', item.name)
                 ]);
             });
 
-           return h('div.project-bin', {attributes: {class: isDraghovering ? 'project-bin--hovering' : ''}}, [
+            if (items.length === 0)
+                vItems = h('div', 'drop files here');
+
+           return h('div', {attributes: {class: isDraghovering ? 'project-bin--hovering project-bin' : 'project-bin'}}, [
                h('div.project-bin__items', [vItems])
            ]);
         });
@@ -64,3 +90,4 @@ function app(interactions){
 }
 
 Cycle.applyToDOM('.js-container', app);
+console.log("app initialized");
