b1df43fe9adf2ae3ba76161ff319367bf1355392
Daniel Bachler <daniel@danielbachler.de>
2015-05-11 00:05:07 +0200

    draft to move project-item into it's own custom element and deal with the nested observables there.

--- js/cycleDnD.js
+++ js/cycleDnD.js
@@ -3,6 +3,36 @@ import cuid from 'cuid';
 const {h, Rx} = Cycle;
 import R from 'ramda';
 
+Cycle.registerCustomElement('project-item', function (interactions, props) {
+    var asset = props.get('asset');
+    var vtree$ = Rx.Observable.combineLatest(asset.progress$, asset.loadedImage$, asset.error$, function (progress, loadedImage, error) {
+        let content;
+
+        if (loadedImage !== null) {
+            content = [
+                h('img.project-item__image', {src: loadedImage}),
+                h('div.project-item__name', asset.name)
+            ];
+        }
+        else if (error !== null) {
+            content = [
+                h('div', 'error')
+            ];
+        }
+        else {
+            content = [
+                h('div.project-item__progress', {style: {width: (progress * 100)}})
+            ];
+        }
+
+        return h('div.project-item', {key: asset.id}, content);
+    });
+
+    return {
+        vtree$: vtree$
+    };
+});
+
 function intent(interactions) {
     return {
         dragEnter$: interactions.get('.project-bin', 'dragenter')
@@ -49,10 +79,19 @@ function model(intent) {
                 id: cuid(),
                 name: file.name,
                 progress$: Rx.Observable.fromEvent(fileReader.onprogress)
-                                        .map(e => e.progress)
+                                        .map(e => {
+                                                if (e.lengthComputable)
+                                                    return (evt.loaded / evt.total);
+                                                else
+                                                    return 0;
+                                            })
                                         .startWith(0),
-                fileLoad$: Rx.Observable.fromEvent(fileReader.onload),
-                fileError$: Rx.Observable.fromEvent(fileReader.onerror)
+                loadedImage$: Rx.Observable.fromEvent(fileReader.onload)
+                                        .map(e => e.target.result)
+                                        .startWith(null),
+                error$: Rx.Observable.fromEvent(fileReader.onerror)
+                                        .map(e => e.target.error)
+                                        .startWith(null)
             };
             fileReader.readAsDataURL(file);
             console.log(`id: ${asset.id}, name: ${asset.name}`);
@@ -77,15 +116,11 @@ function view(model) {
     return Rx.Observable.combineLatest(model.isDragHovering$,
                                        model.items$,
         (isDraghovering, items) => {
-            let vItems = items.map(item => {
-                return h('div.project-item', {key: item.id}, [
-                    h('img.project-item__image'),
-                    h('div.project-item__name', item.name)
-                ]);
-            });
-
+            let vItems;
             if (items.length === 0)
                 vItems = h('div', 'drop files here');
+            else
+                vItems = items.map(item => h('project-item', {asset: item}));
 
            return h('div', {attributes: {class: isDraghovering ? 'project-bin--hovering project-bin' : 'project-bin'}}, [
                h('div.project-bin__items', [vItems])
